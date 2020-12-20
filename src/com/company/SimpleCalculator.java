5b03e5411d5ff2d4b857344e46d5c2167ecda24d
Alexi Krol <krol.mats@gmail.com>
2020-12-20 18:52:35 +0200

    create calculater

---
+++ src/com/company/SimpleCalculator.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class SimpleCalculator {
+
+
+    double firstNumber;
+    double secondNumber;
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public double getAdditionResult() {
+
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber != 0) {
+            return firstNumber / secondNumber;
+        } else {
+            return 0;
+        }
+    }
+
+}
+
+
+
+
+
